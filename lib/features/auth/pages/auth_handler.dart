6e135905225e13f172175c8f493b43e7a01956ef
Tesfahun Kebede <tesfahunkebede336@gmail.com>
2023-07-17 10:14:52 +0300

    create home page

--- lib/features/auth/pages/auth_handler.dart
+++ lib/features/auth/pages/auth_handler.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:sw_and_rick_morty_app/features/auth/data/providers/provider.dart';
 import 'package:sw_and_rick_morty_app/features/auth/pages/splash_screen.dart';
+import 'package:sw_and_rick_morty_app/features/characters/pages/home.dart';
 
 class AuthHandler extends ConsumerWidget {
   const AuthHandler({Key? key}) : super(key: key);
@@ -15,7 +16,7 @@ class AuthHandler extends ConsumerWidget {
         if (authState.loading) {
           return const SplashScreen();
         } else {
-          return Container();
+          return const HomePage();
         }
       }),
     );
